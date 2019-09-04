f552b9d87c30b4459bc637b8e89b8714e6d869e6
umer <umermalik4715@gmail.com>
2019-09-03 22:41:34 -0700

    youtube page update

--- resources/views/youtube.blade.php
+++ resources/views/youtube.blade.php
@@ -6,8 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {{--    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet"--}}
-    {{--          type="text/css">--}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
@@ -22,12 +20,24 @@
     <script type="text/javascript">
 
         function openNav() {
-            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementById("mySidenav").style.width = "80px";
+            document.getElementById("main").style.marginLeft= "130px";
+            document.getElementById("left").classList.add('not-active');
+
+            document.getElementById("toggle-myBtn-full-screen-full").classList.add('not-active');
+            document.getElementById("toggle-myBtn-full-screen-mini").classList.remove('not-active');
+
         }
 
         function closeNav() {
             document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+            document.getElementById("left").classList.remove('not-active');
+
+            document.getElementById("toggle-myBtn-full-screen-mini").classList.add('not-active');
+            document.getElementById("toggle-myBtn-full-screen-full").classList.remove('not-active');
         }
+
 
         function openSearch() {
             document.getElementById("myOverlay").style.display = "block";
@@ -37,6 +47,14 @@
             document.getElementById("myOverlay").style.display = "none";
         }
 
+
+        $(document).ready(function () {
+            $('.left-1st a').click(function () {
+                $('.left-1st a.active').removeClass('active');
+                $(this).addClass('active');
+            });
+        });
+
     </script>
 </head>
 
@@ -44,7 +62,7 @@
 
 <div class="container-fluid">
 
-{{--    OverLay search --}}
+    {{--    OverLay search --}}
     <div id="myOverlay" class="overlay">
         <span class="closebtn" onclick="closeSearch()" title="Close Overlay">×</span>
         <div class="overlay-content">
@@ -59,161 +77,132 @@
     {{--    Togglable side menu--}}
 
     <div id="mySidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+{{--        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>--}}
 
         <div class="col-12 left-1st">
-            <i class="fas fa-home left-ico"></i><span class="left-span">Home</span>
+            <a href="#" class="active"><i class="fas fa-home collapse-menu"></i><br><p class="collaspe-menu-text">Home</p></a>
         </div>
         <div class="col-12 left-1st">
-            <i class="fas fa-fire left-ico"></i><span class="left-span">Trending</span>
+            <a href="#"><i class="fas fa-fire collapse-menu"></i><br><p class="collaspe-menu-text">Trending</p></a>
         </div>
         <div class="col-12 left-1st">
-            <i class="fab fa-firstdraft left-ico"></i><span class="left-span">Subscriptions</span>
-        </div>
-        <hr style="border-width: 3px;">
-        <div class="col-12 left-1st">
-            <i class="fas fa-folder left-ico"></i><span class="left-span">Library</span>
+            <a href="#"><i class="fab fa-firstdraft collapse-menu"></i><br><p class="collaspe-menu-text">Subscriptions</p></a>
         </div>
         <div class="col-12 left-1st">
-            <i class="fas fa-history left-ico"></i><span class="left-span">History</span>
-        </div>
-        <hr style="border-width: 3px;">
-        <div class="col-12">
-            <p>Sign in to like the videos, comment and subscribe</p>
-            <button href="#" id="sign-in-btn-2"><i class="fas fa-user"></i> sign in</button>
-        </div>
-        <hr style="border-width: 3px;">
-        <h4>Best of youtube</h4>
-        <div class="col-12 left-1st">
-            <i class="fas fa-music left-ico"></i><span class="left-span">music</span>
+            <a href="#"><i class="fas fa-folder collapse-menu"></i><br><p class="collaspe-menu-text">Library</p></a>
         </div>
         <div class="col-12 left-1st">
-            <i class="fas fa-volleyball-ball left-ico"></i><span class="left-span">sports</span>
-        </div>
-        <div class="col-12 left-1st">
-            <i class="fas fa-dragon left-ico"></i><span class="left-span">Gaming</span>
-        </div>
-        <div class="col-12 left-1st">
-            <i class="fas fa-history left-ico"></i><span class="left-span">History</span>
-        </div>
-        <div class="col-12 left-1st">
-            <i class="fas fa-music left-ico"></i><span class="left-span">music</span>
-        </div>
-        <div class="col-12 left-1st">
-            <i class="fas fa-volleyball-ball left-ico"></i><span class="left-span">sports</span>
-        </div>
-        <div class="col-12 left-1st">
-            <i class="fas fa-dragon left-ico"></i><span class="left-span">Gaming</span>
-        </div>
-        <div class="col-12 left-1st">
-            <i class="fas fa-history left-ico"></i><span class="left-span">History</span>
-        </div>
-        <hr style="border-width: 3px;">
-        <div class="col-12">
-            <p>Copyright all rights reserved</p>
+            <a href="#"><i class="fas fa-history collapse-menu"></i><br><p class="collaspe-menu-text">History</p></a>
         </div>
 
 
     </div>
 
+    {{--toggle side menu end--}}
+
+
 
     <div class="row">
-
-        <nav class="navbar navbar-default head">
+        <nav class="navbar navbar-fixed-top head">
             <div class="container-fluid">
-                <div class="navbar-header col-md-3">
-                    <a class="navbar-brand" href="#"> <span style="font-size:25px;cursor:pointer; margin-right: 12px;"
-                                                            onclick="openNav()" id="toggle-myBtn">&#9776;</span><i
-                            class="fab fa-youtube" style="font-size: 25px;"></i><b>Youtube</b></a>
-                    <button href="#" class="openBtn" onclick="openSearch()" id="search-btn-2"><i class="fas fa-search"></i></button>
+                <div class="navbar-header col-md-2">
+                    <a class="navbar-brand" href="#" style="padding-top: 8px;">
+                        <span style="font-size:25px;cursor:pointer; margin-right: 12px;" onclick="openNav()"
+                              id="toggle-myBtn">&#9776;</span>
+{{--                        could also use toggle class--}}
+                        <span class="" style="font-size:25px;cursor:pointer; margin-right: 12px;" onclick="openNav()" id="toggle-myBtn-full-screen-full">&#9776;</span>
+{{--                        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>--}}
+                        <span class="not-active" style="font-size:25px;cursor:pointer; margin-right: 12px;" onclick="closeNav()" id="toggle-myBtn-full-screen-mini">&#9776;</span>
+                        <i class="fab fa-youtube" style="font-size: 25px; color: red;"><b style="color: black">YouTube</b></i>
+
+                    </a>
+                    <button href="#" class="openBtn" onclick="openSearch()" id="search-btn-2">
+                        <i class="fas fa-search"></i>
+                    </button>
                 </div>
-                <div class="col-md-4">
-                    <span><input type="text" value="search" id="upper-search"></span>
-                    <button href="#" id="search-btn"><i class="fas fa-search"></i></button>
+                <div class="col-md-7">
+                    <div class="input-group">
+                        <input type="hidden" name="search_param" value="all" id="search_param">
+                        <input type="text" class="form-control" name="x" id="upper-search" placeholder="Search">
+                        <span class="input-group-btn">
+	                        <button class="btn btn-default" type="button" id="search-btn">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                         </span>
+                    </div>
                 </div>
-                <ul class="nav navbar-nav col-md-3 ok">
-                    <li><span class="upper-span"><i class="fas fa-video"></i></span></li>
-                    <li><span class="upper-span"><i class="far fa-share-square"></i></span></li>
-                    <li><span class="upper-span"><i class="fas fa-sliders-h"></i></span></li>
-                    <li><span class="upper-span"><i class="fas fa-ellipsis-v"></i></span></li>
-                </ul>
-                <div class="col-md-2">
+                <ul class="nav navbar-nav col-md-3">
+                    <li><span class="upper-span"><i class="fas fa-video ok"></i></span></li>
+                    <li><span class="upper-span"><i class="far fa-share-square ok"></i></span></li>
+                    <li><span class="upper-span"><i class="fas fa-sliders-h ok"></i></span></li>
+                    <li><span class="upper-span"><i class="fas fa-ellipsis-v ok"></i></span></li>
                     <button href="#" id="sign-in-btn"><i class="fas fa-user"></i> sign in</button>
-                </div>
+                </ul>
             </div>
         </nav>
 
     </div>
 
+
     <div class="row">
 
-        <div class="col-md-3 col-lg-3 left" id="left">
-            <div class="col-12 left-1st">
-                <i class="fas fa-home left-ico"></i><span class="left-span">Home</span>
-            </div>
-            <div class="col-12 left-1st">
-                <i class="fas fa-fire left-ico"></i><span class="left-span">Trending</span>
-            </div>
-            <div class="col-12 left-1st">
-                <i class="fab fa-firstdraft left-ico"></i><span class="left-span">Subscriptions</span>
-            </div>
-            <hr style="border-width: 3px;">
-            <div class="col-12 left-1st">
-                <i class="fas fa-folder left-ico"></i><span class="left-span">Library</span>
-            </div>
-            <div class="col-12 left-1st">
-                <i class="fas fa-history left-ico"></i><span class="left-span">History</span>
-            </div>
-            <hr style="border-width: 3px;">
-            <div class="col-12">
-                <p>Sign in to like the videos, comment and subscribe</p>
-                <button href="#" id="sign-in-btn"><i class="fas fa-user"></i> sign in</button>
-            </div>
-            <hr style="border-width: 3px;">
-            <h4>Best of youtube</h4>
-            <div class="col-12 left-1st">
-                <i class="fas fa-music left-ico"></i><span class="left-span">music</span>
-            </div>
-            <div class="col-12 left-1st">
-                <i class="fas fa-volleyball-ball left-ico"></i><span class="left-span">sports</span>
-            </div>
-            <div class="col-12 left-1st">
-                <i class="fas fa-dragon left-ico"></i><span class="left-span">Gaming</span>
-            </div>
-            <div class="col-12 left-1st">
-                <i class="fas fa-history left-ico"></i><span class="left-span">History</span>
-            </div>
-            <div class="col-12 left-1st">
-                <i class="fas fa-music left-ico"></i><span class="left-span">music</span>
-            </div>
-            <div class="col-12 left-1st">
-                <i class="fas fa-volleyball-ball left-ico"></i><span class="left-span">sports</span>
-            </div>
-            <div class="col-12 left-1st">
-                <i class="fas fa-dragon left-ico"></i><span class="left-span">Gaming</span>
-            </div>
-            <div class="col-12 left-1st">
-                <i class="fas fa-history left-ico"></i><span class="left-span">History</span>
-            </div>
-            <hr style="border-width: 3px;">
-            <div class="col-12">
-                <p>Copyright all rights reserved</p>
-            </div>
+                <div class="col-md-3 left" id="left">
+                    <div class="col-12 left-1st">
+                       <a href="#" class="active"> <i class="fas fa-home left-ico"></i><span class="left-span">Home</span> </a>
+                    </div>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-fire left-ico"></i><span class="left-span">Trending</span></a>
+                    </div>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fab fa-firstdraft left-ico"></i><span class="left-span">Subscriptions</span></a>
+                    </div>
+                    <hr style="border-width: 3px;">
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-folder left-ico"></i><span class="left-span">Library</span></a>
+                    </div>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-history left-ico"></i><span class="left-span">History</span></a>
+                    </div>
+                    <hr style="border-width: 3px;">
+                    <div class="col-12">
+                        <p>Sign in to like the videos, comment and subscribe</p>
+                        <button href="#" id="sign-in-btn"><i class="fas fa-user"></i> sign in</button>
+                    </div>
+                    <hr style="border-width: 3px;">
+                    <h4>Best of youtube</h4>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-music left-ico"></i><span class="left-span">music</span></a>
+                    </div>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-volleyball-ball left-ico"></i><span class="left-span">sports</span></a>
+                    </div>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-dragon left-ico"></i><span class="left-span">Gaming</span></a>
+                    </div>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-history left-ico"></i><span class="left-span">History</span></a>
+                    </div>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-music left-ico"></i><span class="left-span">music</span></a>
+                    </div>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-volleyball-ball left-ico"></i><span class="left-span">sports</span></a>
+                    </div>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-dragon left-ico"></i><span class="left-span">Gaming</span></a>
+                    </div>
+                    <div class="col-12 left-1st">
+                        <a href="#"><i class="fas fa-history left-ico"></i><span class="left-span">History</span></a>
+                    </div>
+                    <hr style="border-width: 3px;">
+                    <div class="col-12">
+                        <p>Copyright all rights reserved.</p>
+                    </div>
+                </div>
 
-        </div>
 
 
-        <div class="col-md-9 right">
-
-            {{--            <div class="row">--}}
-            {{--                <div class="col-md-8">--}}
-            {{--                    <a href="#"><img src="https://via.placeholder.com/600x240" alt="video thumb" class="video-thumbnail"></a>--}}
-            {{--                </div>--}}
-            {{--                <div class="col-md-4" >--}}
-            {{--                    <a href="#"><img src="https://via.placeholder.com/300x240" alt="video thumb" class="video-thumbnail"></a>--}}
-            {{--                </div>--}}
-            {{--            </div>--}}
-
+        <div class="col-md-9 right" id="main">
 
             <div class="row">
                 <h4 class="video-heading">Trending</h4>
@@ -311,6 +300,7 @@
 
         </div>
     </div>
+
 
 </div>
 
